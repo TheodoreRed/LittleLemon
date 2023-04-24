13a7ee93688a68969585af49bc1160ba80f9d07e
Theodore Redlon <redlontheo@gmail.com>
2023-04-24 09:39:28 -0400

    Set up MySQL connection and Models

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField(validators=[MaxValueValidator(6)])
+    date = models.DateTimeField()
+
+    def __str__(self):
+       return self.name
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.title
